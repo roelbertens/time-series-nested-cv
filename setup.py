ad75f3b119b5868e1ac534fe4e4f778739d4a9bf
roelbertens <roelbertens@users.noreply.github.com>
2019-02-08 17:09:08 +0100

    renaming and update notebook

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ import setuptools
 requirements = ['numpy', 'pandas']
 
 setuptools.setup(
-    name="timeseries_cross_validation",
+    name="time_series_cross_validation",
     author="Roel Bertens",
     author_email="roelbertens@godatadriven.com",
     description="""A cross-validation splitter for time series data.""",
     license="MIT",
-    packages=setuptools.find_packages("timeseries_cross_validation"),
+    packages=setuptools.find_packages("time_series_cross_validation"),
     version="0.1.0",
     install_requires=requirements,
 )
